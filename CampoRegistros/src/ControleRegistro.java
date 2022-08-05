0192b61d2b948566d2fe7e4bd90ffce755e55be1
barroscruzc <barroscruzc@gmail.com>
2022-08-05 11:35:41 -0300

    teste usando gitBash

---
+++ CampoRegistros/src/ControleRegistro.java
@@ -0,0 +1,11 @@
+
+public class ControleRegistro {
+
+	public static void main(String[] args) {
+		//Criando uma variável do tipo Array
+		
+		String[] carros = {"Uno", "HB20", "Ka"};
+		System.out.println(carros[0]);
+	}
+
+}
