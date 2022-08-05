62eb6f5e5c6016e9a9962ff78acbefc9d08861a7
barroscruzc <barroscruzc@gmail.com>
2022-08-05 11:44:17 -0300

    teste usando gitBash

--- CampoRegistros/src/ControleRegistro.java
+++ CampoRegistros/src/ControleRegistro.java
@@ -4,8 +4,14 @@ public class ControleRegistro {
 	public static void main(String[] args) {
 		//Criando uma variável do tipo Array
 		
-		String[] carros = {"Uno", "HB20", "Ka"};
-		System.out.println(carros[0]);
+		String[] carros = {"Uno", "HB20", "Ka", "Gol", "Onix", "BMW"};
+		int i = 0;
+		System.out.println("Lista de Carros");
+		while (i < carros.length) {
+			System.out.println((i+1) + "º : " + carros[i]);
+			i++;
+		}
+		
 	}
 
 }
