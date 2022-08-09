d5c9e69003e74fff1397ec6b96fd2eba33a6e4c7
barroscruzc <barroscruzc@gmail.com>
2022-08-09 09:21:38 -0300

    Aula de introdução a POO - Métodos e Encapsulamento git push

--- ProjetoComercioEletronico/src/Loja.java
+++ ProjetoComercioEletronico/src/Loja.java
@@ -5,29 +5,46 @@ public class Loja {
 		
 		Produto p; //Declarando o objeto
 		p = new Produto(); //Reservando (alocar) a memória
-		p.codigo = 123;
-		p.descricao = "computador";
-		p.preco = 2500.00;
+		p.setCodigo(123);
+		p.setDescricao("computador");
+		p.setPreco(2500.00);
 		
 		Produto p2;
 		p2 = new Produto(); 
-		p2.codigo = 456;
-		p2.descricao = "Cadeira escritório";
-		p2.preco = 700.00;
+		p2.setCodigo(456);
+		p2.setDescricao("Cadeira escritório");
+		p2.setPreco(700.00);
 		
 		Produto p3;
 		p3 = new Produto(); 
-		p3.codigo = 789;
-		p3.descricao = "Teclado";
-		p3.preco = 150.00;
+		p3.setCodigo(789);
+		p3.setDescricao("Teclado");
+		p3.setPreco(150.00);
 		
 
 		
 		//invocando o método mostrarAnuncio para cada objeto da classe Produto criado acima
+		System.out.println("----- Preços antes dos descontos -----");
+		p.mostrarAnuncio();
+		p2.mostrarAnuncio();
+		p3.mostrarAnuncio();
+		
+		System.out.println("***************************************");
+		System.out.println("************* Novos preços ************");
+		p.aplicarDesconto(10);
+		p2.modificarPreco(550.00);
+		p3.modificarPreco(99.99);
+		
 		p.mostrarAnuncio();
 		p2.mostrarAnuncio();
 		p3.mostrarAnuncio();
 
+		
+		//simulando descontos
+		double precoP = p.simularDesconto(15);
+		System.out.println("O preço do "+ p.getDescricao() + " com mais 15% de desconto fica: R$" + precoP);
+		//Ao mostrar o anúncio, o preco do Produto p continua inalterado
+		p.mostrarAnuncio();
 	}
 
 }
--- ProjetoComercioEletronico/src/Produto.java
+++ ProjetoComercioEletronico/src/Produto.java
@@ -5,11 +5,40 @@
 public class Produto {
 
 	/* Atributos
-	 * é necessário declarar as variáveis que compõem a estrutura dessa classe*/
-	int codigo;
-	String descricao;
-	double preco;
+	 * é necessário declarar as variáveis que compõem a estrutura dessa classe
+	 * Encapsulamento
+	 * ao deixar os atributos privados, somente é possível acessar e modificar essas informações 
+	 * com métodos da própria classe, tornando a aplicação mais segura*/
+	private int codigo;
+	private String descricao;
+	private double preco;
 	
+	//Definindo os getters e setters (métodos para setar ou recuperar as informações contidas nos atributos	
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	//Métodos Void (sem retorno)
 	//Método que imprime na tela os atributos do produto
 	void mostrarAnuncio () {
 		System.out.println("------ PRODUTO EM OFERTA ------");
@@ -17,4 +46,19 @@ public class Produto {
 		System.out.printf("R$ %.2f\n", preco);
 	}
 	
+	//métodos que recebem entradas (parâmetros)
+	void modificarPreco(double novoPreco) {
+		preco = novoPreco;
+	}
+	
+	void aplicarDesconto(double percentual) {
+		preco = preco - preco * percentual / 100;
+	}
+	
+	//método com retorno
+	double simularDesconto(double percentual) {
+		double precoSimulado;
+		precoSimulado = preco - preco * percentual / 100;
+		return precoSimulado;
+	}
 }
